26e0f0c41713fb23f124fb9905cfb8945f3a3a6e
Duverney muriel <duverwild@gmail.com>
2025-12-13 21:50:27 +0100

    Initial Commit

---
+++ comfortaapp/comfortaappApp.swift
@@ -0,0 +1,17 @@
+//
+//  comfortaappApp.swift
+//  comfortaapp
+//
+//  Created by duverney muriel on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct comfortaappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
